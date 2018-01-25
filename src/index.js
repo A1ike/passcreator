ff0a16062872c67d7c2aa05a28f0f7e321d5460c
Dmitry Katyushin <a1ike28@gmail.com>
2018-01-25 20:45:02 +0300

    init project

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/*!
+ * pass-creator <https://github.com/a1ike/pass-creator>
+ *
+ * Copyright (c) 2018, Dmitry Katyushin.
+ * Released under the MIT License.
+ */
+
+/**
+ * special: '~!@#$%^&()_+-={}[];\',.'
+ */
+
+/**
+ * Description
+ *
+ * @param {String} `param` Bla bla bla.
+ * @return {String}
+ * @api public
+ */
+
+function passCreator() {
+  return 'Hello';
+}
+
+export default passCreator;
